519086da5b1b897c48050e9e3a7ed0a54c5d06e4
KhanAfsar12 <ka484564@gmail.com>
2025-01-18 21:18:01 +0530

    single file code updated with MVC by using Blueprint

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+class Config:
+    SECRET_KEY = 'afsar_khan2121'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    MAIL_SERVER = "smtp.gmail.com",
+    MAIL_PORT = 587,
+    MAIL_USERNAME = "ka484564@gmail.com",
+    MAIL_PASSWORD = "nwjq ltzp lcpm raxc",
+    MAIL_USE_TLS = True,
+    MAIL_DEBUG = True
